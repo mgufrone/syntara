189a6d5a6e9a8b1269057a1f628b11b1c3999cf2
Mochamad Gufron <mgufronefendi@gmail.com>
2014-08-06 08:08:39 +0700

    add indonesian translation

--- src/lang/el/all.php
+++ src/lang/el/all.php
@@ -21,6 +21,14 @@ return array(
         'banned' => 'Ο χρήστης αποκλείστηκε, παρακαλώ ενημερώστε τον διαχειριστή.',
         'login-failed' => 'Μας συγχωρείτε, η σύνδεση απέτυχε... ελέγξε τα στοιχεία της σύνδεσης σας.',
         'denied' => 'Μας συγχωρείτε, δεν έχετε δικαίωμα πρόσβασης !',
+        'error'=>array(
+            '404' => 'We kunnen \' t vinden de pagina die u zoekt ',
+            '404-title' => '404. Niet gevonden ',
+            '403' => 'We zijn zo droevig, maar u bent niet toegestaan ​​om deze pagina te bekijken',
+            '403-title' => '403. Toegang geweigerd ',
+            '500' => 'We ondervonden een aantal problemen. Sorry. :-('
+            '500-title' => '500. Internal Server Error ', 
+        ),
     ),
     'titles' => array(
         'index' => 'Ταμπλό',
---
+++ src/lang/id/all.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+    'remember' => 'ingat saya',
+    'signin' => 'Masuk',
+    'email' => 'Email',
+    'password' => 'Kata Sandi',
+    'error' => 'Error !',
+    'search' => 'Cari',
+    'confirm-delete-title' => 'Konfirmasi hapus',
+    'confirm-delete-message' => 'Anda yakin untuk menghapus item ini?',
+    'delete' => 'Hapus',
+    'new' => 'Baru',
+    'name' => 'Nama',
+    'show' => 'Tampilkan',
+    'create' => 'Buat',
+    'update' => 'Ubah',
+    'yes' => 'Ya',
+    'no' => 'Tidak',
+    'messages' => array(
+        'banned' => 'Pengguna tidak diijinkan, segera hubungi administrator.',
+        'login-failed' => 'Maaf, gagal untuk masuk... Cek kembali akun anda.',
+        'denied' => 'Maaf, Akses ditolak !',
+        'error'=>array(
+            '404'=>'Kami tidak dapat menemukan halaman yang sedang anda cari',
+            '404-title'=>'404. Not Found',
+            '403'=>'Kami mohon maaf, tapi anda tidak diperbolehkan untuk mengakses halaman berikut',
+            '403-title'=>'403. Akses Ditolak',
+            '500'=>'Kami sedang mengalami gangguan. Mohon Maaf. :-(',
+            '500-title'=>'500. Gangguan Server',
+        ),
+    ),
+    'titles' => array(
+        'index' => 'Dashboard',
+        'login' => 'Masuk',
+        'error' => 'Error',
+    ),
+);
